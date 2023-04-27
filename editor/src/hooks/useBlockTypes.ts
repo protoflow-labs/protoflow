83f9112d8384e288180a113b642fef7e909b222e
Nolan Leung <leung.nolan@gmail.com>
2023-04-26 20:59:54 -0700

    rename nodeType -> blockType

--- editor/src/hooks/useBlockTypes.ts
+++ editor/src/hooks/useBlockTypes.ts
@@ -1,5 +1,4 @@
 import { BucketBlock } from "@/components/blocks/BucketBlock";
-import { EmailBlock } from "@/components/blocks/EmailBlock";
 import { EntityBlock } from "@/components/blocks/EntityBlock";
 import { FunctionBlock } from "@/components/blocks/FunctionBlock";
 import { InputBlock } from "@/components/blocks/InputBlock";
@@ -7,8 +6,8 @@ import { QueryBlock } from "@/components/blocks/QueryBlock";
 import { QueueBlock } from "@/components/blocks/QueueBlock";
 import { useMemo } from "react";
 
-export function useNodeTypes() {
-  const nodeTypes = useMemo(
+export function useBlockTypes() {
+  const blockTypes = useMemo(
     () => ({
       "protoflow.entity": EntityBlock,
       "protoflow.function": FunctionBlock,
@@ -16,10 +15,9 @@ export function useNodeTypes() {
       "protoflow.query": QueryBlock,
       "protoflow.queue": QueueBlock,
       "protoflow.bucket": BucketBlock,
-      "protoflow.email": EmailBlock,
     }),
     []
   );
 
-  return { nodeTypes };
+  return { blockTypes };
 }
