bdad31be0ca1bfa2586ae5d466c26c6390bf907e
Nolan Leung <leung.nolan@gmail.com>
2023-04-27 08:44:14 -0700

    fix import

--- editor/src/components/blockEditors/CollectionEditor.tsx
+++ editor/src/components/blockEditors/CollectionEditor.tsx
@@ -1,7 +1,7 @@
+import { Divider, Field, Input } from "@fluentui/react-components";
 import { useForm } from "react-hook-form";
 import { Node } from "reactflow";
 import { EditorActions, useUnselect } from "../EditorActions";
-import {Field, Input} from "@fluentui/react-components";
 
 export type EntityData = {
   name: string;
